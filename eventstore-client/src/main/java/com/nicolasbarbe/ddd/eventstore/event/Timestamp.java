0886f6f939997e7e895b8bd8cd897d0c7f04214c
Nicolas Barbé <nicolas.barbe@magnolia-cms.com>
2018-06-21 21:38:03 +0200

    Add simple k8s deployment

--- eventstore-client/src/main/java/com/nicolasbarbe/ddd/eventstore/event/Timestamp.java
+++ eventstore-client/src/main/java/com/nicolasbarbe/ddd/eventstore/event/Timestamp.java
@@ -4,6 +4,8 @@ package com.nicolasbarbe.ddd.eventstore.event;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+// todo move timestamp to server, otherwise, there is no guarantee timestamps will be in the correct order
+// however, it also means we assume events created by commands have to be emitted immediately 
 public class Timestamp {
     public static String now() {
         return new SimpleDateFormat("yyyy-MM-dd'T'h:m:ssZZZZZ").format(new Date());
